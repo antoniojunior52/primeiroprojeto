6e557b6df0737c314698834a7f8824dcc061fca2
Antonio Junior <antoniocarlosjuninhojuca@gmail.com>
2021-10-05 18:38:28 -0300

    primeiroprojeto

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿ using  System ;
+
+namespace  meuprimeiroprojto
+{
+     programa de aula
+    {
+        static  void  Main ( string [] args )
+        {
+               string [] alunosams =  nova  string [ 5 ] {
+               
+               " Dani " ,
+               " Maju " ,
+               " Bruno " ,
+               " Ramon " ,
+               " Sofi "
+            };
+           Console . WriteLine ( alunosams [ 2 ]);
+
+            for ( int  i  =  0 ; i  <  alunosams . Comprimento ; i ++ )
+            {
+                Console . WriteLine ( $ " Os alunos chamados são: { alunosams [ i ]} " );
+            }
+        }
+    }
+}
